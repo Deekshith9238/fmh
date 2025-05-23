c52a79dbfe96a2906d17f488326d7df5f8c33478
deekshith-h-u <deekshith9238@gmail.com>
2025-05-23 20:46:48 +0530

    databse issue

--- server/db.ts
+++ server/db.ts
@@ -11,5 +11,11 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set.");
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 export const db = drizzle(pool, { schema });
